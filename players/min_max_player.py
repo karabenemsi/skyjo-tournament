bc5824516e5c901740f7f84ce5ee85a308a0a53f
karabenemsi <florian@florianlubitz.de>
2024-01-02 00:51:23 +0100

    add min max player

---
+++ players/min_max_player.py
@@ -0,0 +1,59 @@
+from players.player import Player
+from card import Card
+import random
+from skyjo_game import SkyJoGame
+
+
+class MinMaxPlayer(Player):
+    def start_round(self):
+        # flip 2 random cards
+        cards_to_flip = random.sample(range(0, len(self._cards)), 2)
+        for card_index in cards_to_flip:
+            self._cards[card_index].flip()
+
+    def draw_card(self, game: SkyJoGame):
+        highest_card_value = max(
+            [r.get_value() or -10 for r in self._cards if r is not None]
+        )
+        if highest_card_value > game.get_top_discard_card().get_value():
+            self._card_in_hand = game.pop_top_discard_card()
+        else:
+            self._card_in_hand = game.pop_top_card()
+
+    def discard_card(self, game: SkyJoGame) -> Card:
+        if self._card_in_hand is None:
+            raise ValueError("No card in hand")
+        card_indices_with_higher_value = [
+            index
+            for index, card in enumerate(self._cards)
+            if card is not None
+            and card.get_value() is not None
+            and card.get_value() >= self._card_in_hand.get_value()
+        ]
+        if len(card_indices_with_higher_value) > 0:
+            index = random.choice(card_indices_with_higher_value)
+            temp = self._cards[index]
+            self._cards[index] = self._card_in_hand
+            self._card_in_hand = None
+            return temp
+
+        # If no card is with value higher than the card in hand, discard the card in hand or switch with uncovered card
+        # Indeces of all invisible cards
+        invisible_cards_indices = [
+            index
+            for index, card in enumerate(self._cards)
+            if card is not None and card.get_value() is None
+        ]
+
+        if self._card_in_hand.get_value() > 6:
+            # Flip a random invisible card and discard the card in hand
+            self._cards[random.choice(invisible_cards_indices)].flip()
+            return self._card_in_hand
+        else:
+            if len(invisible_cards_indices) == 0:
+                raise ValueError("No invisible cards")
+            # Select random invisible card to switch with
+            index_of_card_to_switch = random.choice(invisible_cards_indices)
+            temp = self._cards[index_of_card_to_switch]
+            self._cards[index_of_card_to_switch] = self._card_in_hand
+            return temp
