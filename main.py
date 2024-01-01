a593cd31a5b14dd0caa84268cc6eb937ef56b0f9
karabenemsi <florian@florianlubitz.de>
2024-01-02 00:54:43 +0100

    testing env

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 from skyjo_game import SkyJoGame
 from players.random_player import RandomPlayer
+from players.min_max_player import MinMaxPlayer
 
 if __name__ == "__main__":
-    players = [RandomPlayer(0), RandomPlayer(1)]
-    game = SkyJoGame(players)
-    game.play_game()
+    players = [
+        RandomPlayer("Random Jürgen"),
+        RandomPlayer("Random Peter"),
+        MinMaxPlayer("MinMax Joachim"),
+    ]
+    scores = {player.id: 0 for player in players}
+    for i in range(1000):
+        game = SkyJoGame(players)
+        winner = game.play_game()
+        scores[winner] += 1
+
+    print(scores)
